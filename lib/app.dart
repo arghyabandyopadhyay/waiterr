18c399e22c879cc521321dd2c19492d143e0fc84
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-18 16:26:12 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:waiterr/Pages/Login/splash_page.dart';
+import 'package:waiterr/theme.dart';
 import '../Pages/Login/sign_up_page.dart';
 import 'global_class.dart';
 import '../stores/login_store.dart';
@@ -85,7 +86,21 @@ class MyApp extends StatelessWidget {
                         // Define a light and dark color theme. Then, read the user's
                         // preferred ThemeMode (light, dark, or system default) from the
                         // SettingsController to display the correct theme.
-                        theme: ThemeData(),
+                        theme: ThemeData(
+                            scaffoldBackgroundColor:
+                                GlobalTheme.backgroundColor,
+                            primaryColorLight: GlobalTheme.primaryColorLight,
+                            appBarTheme: const AppBarTheme(
+                                elevation: 0,
+                                iconTheme: IconThemeData(
+                                    color: GlobalTheme.appBarIconColor),
+                                backgroundColor: Colors.transparent),
+                            floatingActionButtonTheme:
+                                const FloatingActionButtonThemeData(
+                                    backgroundColor:
+                                        GlobalTheme.floatingButtonBackground,
+                                    foregroundColor:
+                                        GlobalTheme.floatingButtonText)),
                         darkTheme: ThemeData.dark(),
                         themeMode: settingsController.themeMode,
 
