36466d177319443e300c8fe72836b4d03fe3ee1f
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-18 16:26:44 +0530

    Create bottom_sheet_communication_template.dart

---
+++ lib/Model/bottom_sheet_communication_template.dart
@@ -0,0 +1,8 @@
+import 'customizable_page_model.dart';
+
+class CustValTemp {
+  List<CustomizablePageModel> orderList;
+  double? totalItem;
+  double? totalCartAmount;
+  CustValTemp(this.orderList, this.totalItem, this.totalCartAmount);
+}
