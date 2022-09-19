47231386aaaf07bbd6758b7d36b1a72da444102e
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-19 10:45:31 +0530

    Create theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GlobalTheme {
+  static const Color primaryColor = Color.fromARGB(255, 3, 63, 244);
+  static const Color primaryColorLight = Color.fromARGB(255, 41, 216, 255);
+  static const Color primaryText = Color(0XFF808080);
+  static const Color secondaryText = Colors.black;
+  static const Color floatingButtonText = Colors.white;
+  static const Color floatingButtonBackground = Color.fromARGB(255, 20, 16, 54);
+  static const Color backgroundColorDim = Color(0xFFf5f5f5);
+  static const Color backgroundColor = Color.fromARGB(255, 180, 172, 240);
+  static const Color backgroundColorLoginPage =
+      Color.fromARGB(255, 255, 255, 255);
+  static const Color progressBarBackground = Color.fromARGB(255, 20, 16, 54);
+  static const Color appBarIconColor = Color.fromARGB(255, 20, 16, 54);
+  static var primaryGradient = [
+    const Color.fromARGB(255, 249, 95, 195),
+    const Color.fromARGB(255, 244, 3, 3)
+  ];
+  static var primaryGradient2 = [
+    const Color.fromARGB(255, 118, 106, 228),
+    const Color.fromARGB(255, 72, 65, 141),
+    const Color.fromARGB(255, 20, 16, 54)
+  ];
+  static var buttonGradient = [
+    const Color.fromARGB(255, 202, 16, 165),
+    const Color.fromARGB(255, 231, 35, 35)
+  ];
+}
