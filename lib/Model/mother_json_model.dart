7fd8dd0dcfae0a22095757f39474bdec84c6d8ee
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-20 15:25:50 +0530

    Create mother_json_model.dart

---
+++ lib/Model/mother_json_model.dart
@@ -0,0 +1,22 @@
+class MotherJSONModel {
+  String? masterfield;
+  String? particulars;
+  String? subParticulars;
+  var info;
+  String? subInfo;
+  String? messageType;
+  String? remarks;
+  MotherJSONModel(this.masterfield, this.particulars, this.subParticulars,
+      this.info, this.subInfo, this.messageType, this.remarks);
+
+  factory MotherJSONModel.fromJson(dynamic json) {
+    return MotherJSONModel(
+        json['MasterField'] as String?,
+        json['Particulars'] as String?,
+        json['SubParticulars'] as String?,
+        json['Info'] as dynamic,
+        json['SubInfo'] as String?,
+        json['MessageType'] as String?,
+        json['Remarks'] as String?);
+  }
+}
