0ee730388226a64650dc58775ca7850d288289d5
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-22 19:32:42 +0530

    Create user_restraunt_allocation_model.dart

---
+++ lib/Model/user_restraunt_allocation_model.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+class UserRestrauntAllocationModel {
+  final String? clientName;
+  final String? logoURL;
+  final String? dataExchangeVia;
+  final String? dataExchangeURL;
+  final String? clientType;
+  final String? guid;
+  final String? companyGUID;
+  final List<OutletConfigurationModel>? outletConfiguration;
+
+  UserRestrauntAllocationModel(
+      {this.clientName,
+      this.logoURL,
+      this.dataExchangeVia,
+      this.dataExchangeURL,
+      this.clientType,
+      this.guid,
+      this.companyGUID,
+      this.outletConfiguration});
+  factory UserRestrauntAllocationModel.fromJson(Map<String, dynamic> json) {
+    return UserRestrauntAllocationModel(
+      clientName: json['ClientName'],
+      logoURL: json['LogoURL'],
+      dataExchangeVia: json['DataExchangeVia'],
+      dataExchangeURL: json['DataExchangeUrl'],
+      clientType: json['ClientType'],
+      guid: json['GUID'],
+      companyGUID: json['CompanyGUID'],
+      outletConfiguration: (json['outletConfiguration'] as List)
+          .map((data) => OutletConfigurationModel.fromJson(data))
+          .toList(),
+    );
+  }
+
+  Map toJson() {
+    List<Map>? outletConfiguration = this.outletConfiguration != null
+        ? this.outletConfiguration!.map((i) => i.toJson()).toList()
+        : null;
+    return {
+      'clientName': clientName,
+      'logoURL': logoURL,
+      'dataExchangeVia': dataExchangeVia,
+      'dataExchangeURL': dataExchangeURL,
+      'clientType': clientType,
+      'guid': guid,
+      'companyGUID': companyGUID,
+      'outletConfiguration': outletConfiguration,
+    };
+  }
+}
+
+class OutletConfigurationModel {
+  String? outletName;
+  String? outletSalePoint;
+  OutletConfigurationModel(this.outletName, this.outletSalePoint);
+  factory OutletConfigurationModel.fromJson(Map<String, dynamic> json) {
+    return OutletConfigurationModel(
+        json['OutletName'] as String?, json['OutletSalePoint'] as String?);
+  }
+  Map toJson() => {
+        "OutletName": outletName,
+        "OutletSalePoint": outletSalePoint,
+      };
+}
