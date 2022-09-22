69465907c9299f506d6a7967e150e8158dd01b85
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-22 19:32:29 +0530

    Create user_details_model.dart

---
+++ lib/Model/user_details_model.dart
@@ -0,0 +1,43 @@
+class UserDetailsModel {
+  final String id;
+  final String? name;
+  final String mobileNumber;
+  final int roleID;
+  final bool isActive;
+  final String? profileUrl;
+  String? deviceToken;
+  String? lastLogin;
+
+  UserDetailsModel(
+      {required this.id,
+      this.name,
+      required this.mobileNumber,
+      required this.roleID,
+      required this.isActive,
+      this.profileUrl,
+      this.deviceToken,
+      this.lastLogin});
+
+  factory UserDetailsModel.fromJson(Map<String, dynamic> json) {
+    return UserDetailsModel(
+        id: json['id'],
+        name: json['Name'],
+        mobileNumber: json['MobileNumber'],
+        roleID: json['RoleId'],
+        isActive: json['IsActive'] == 1,
+        profileUrl: json['ProfileURL'],
+        deviceToken: json['DeviceToken'],
+        lastLogin: json['last_login']);
+  }
+
+  Map toJson() => {
+        "id": id,
+        "Name": name,
+        "MobileNumber": mobileNumber,
+        "RoleId": roleID,
+        "IsActive": (isActive) ? 1 : 0,
+        "ProfileURL": profileUrl,
+        "DeviceToken": deviceToken,
+        "last_login": lastLogin
+      };
+}
