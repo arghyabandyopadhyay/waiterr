5f6e0dc9e82ec7b080f14d979d9383e1026312de
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-23 23:23:29 +0530

    Update home_page.dart

--- lib/Pages/Login/home_page.dart
+++ lib/Pages/Login/home_page.dart
@@ -15,8 +15,6 @@ import 'package:waiterr/Pages/User/about_page.dart';
 import 'package:waiterr/Pages/User/profile_page.dart';
 import 'package:waiterr/widgets/drawer.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:waiterr/stores/login_store.dart';
 import 'package:waiterr/theme.dart';
