9e526ee057b13002b4d1124287194e4815d69eae
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-23 22:19:25 +0530

    Create product_specification.dart

---
+++ lib/Pages/Restaurant/Product/product_specification.dart
@@ -0,0 +1,39 @@
+//ProductSpecification
+import 'package:waiterr/global_class.dart';
+import 'package:waiterr/Model/menu_item_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ProductSpecification extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    MenuItemModel? item = UserDetail.item;
+    return ListView(
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Specifications",
+              textScaleFactor: 1,
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+            ),
+            // GestureDetector(
+            //     child: Container(
+            //       padding: EdgeInsets.symmetric(horizontal: 10,vertical: 2),
+            //       decoration:BoxDecoration(color:Colors.white,border: Border.all(color: GlobalTheme.primaryText.withOpacity(0.2),width: 1,style: BorderStyle.solid),),
+            //       child:Text("Rate Product",style: TextStyle(fontSize:15,color: GlobalTheme.primaryColor,fontWeight: FontWeight.bold)),
+            //     ),
+            //     onTap:(){
+            //       Navigator.push(context, CupertinoPageRoute(builder: (context1) => GetRatingAndReviews(item: item,)));
+            //     })
+          ],
+        )
+      ],
+    );
+  }
+}
