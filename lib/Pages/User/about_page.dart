76bb958b282c05cb53d3abfe796473d9fc830706
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-23 23:24:10 +0530

    Update about_page.dart

--- lib/Pages/User/about_page.dart
+++ lib/Pages/User/about_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../theme.dart';
 
