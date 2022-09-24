55480eae0661e5db31a358a7f826a88cc8abf2b4
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-09-24 16:47:49 +0530

    Update customizable_page_model.dart

--- lib/Model/customizable_page_model.dart
+++ lib/Model/customizable_page_model.dart
@@ -1,17 +1,14 @@
 class CustomizablePageModel {
-  String? name;
-  String? qty;
-  String? price;
-  CustomizablePageModel(this.name,this.qty, this.price);
+  String name;
+  double qty;
+  double price;
+  CustomizablePageModel(
+      {required this.name, required this.qty, required this.price});
 
   factory CustomizablePageModel.fromJson(dynamic json) {
-    return CustomizablePageModel(json['Name'] as String?,json['Qty'] as String?, json['Price'] as String?);
+    return CustomizablePageModel(
+        name: json['Name'] ?? "",
+        qty: json['Qty'] ?? 0,
+        price: json['Price'] ?? 0);
   }
-
-  // CustomizablePageModel.fromMap(Map<String, dynamic> map) {
-  //   name: map['Name'];
-  //   qty: map['Qty'];
-  //   price:map['Price'];
-  // }
-
 }
