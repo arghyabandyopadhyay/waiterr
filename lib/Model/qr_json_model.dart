ef082370e777ab22f43275ffe5b05afc9a60dbb6
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-23 21:17:18 +0530

    Update qr_json_model.dart

--- lib/Model/qr_json_model.dart
+++ lib/Model/qr_json_model.dart
@@ -1,20 +1,13 @@
 class QRJSONModel {
-  String loginId;
-  String userPassword;
   String restaurantId;
   String table;
-  String outlet;
-  String salespointType;
-  QRJSONModel(this.loginId, this.userPassword, this.restaurantId, this.table,
-      this.outlet, this.salespointType);
+  String outletName;
+  String outletSalespointType;
+  QRJSONModel(this.restaurantId, this.table, this.outletName,
+      this.outletSalespointType);
 
   factory QRJSONModel.fromJson(dynamic json) {
-    return QRJSONModel(
-        json['LoginId'] as String,
-        json['UserPassword'] as String,
-        json['RestaurantId'] as String,
-        json['Table'] as String,
-        json['Outlet'] as String,
-        json['SalespointType'] as String);
+    return QRJSONModel(json['RestaurantId'] as String, json['Table'] as String,
+        json['Outlet'] as String, json['SalespointType'] as String);
   }
 }
