0ba66eccad14188787122fd1d935e1725078ee24
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-24 03:22:26 +0530

    Create confirmation_dialog.dart

---
+++ lib/widgets/confirmation_dialog.dart
@@ -0,0 +1,106 @@
+//ConfirmationDialog
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../theme.dart';
+
+class ConfirmationDialog extends StatelessWidget {
+  final onTap_Yes;
+  final onTap_No;
+  final question;
+  final headerText;
+  // This widget is the root of your application.
+  const ConfirmationDialog(
+      {Key? key, this.onTap_Yes, this.onTap_No, this.question, this.headerText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0)), //this right here
+      child: Container(
+        height: 160,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  headerText,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Center(
+                child: Text(
+                  question,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(fontSize: 17),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      onTap_No();
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            color: GlobalTheme.primaryText,
+                            width: 1.5,
+                            style: BorderStyle.solid),
+                      ),
+                      alignment: Alignment.center,
+                      height: 40,
+                      width: 80,
+                      child: const Text(
+                        "No",
+                        style: TextStyle(
+                            fontSize: 20, color: GlobalTheme.primaryText),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      onTap_Yes();
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 40,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            color: GlobalTheme.primaryColor,
+                            width: 1.5,
+                            style: BorderStyle.solid),
+                      ),
+                      child: const Text(
+                        "Yes",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: GlobalTheme.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
