05ef560261ce219dbfa45c6ef13d17fa75c1c3a6
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-24 18:50:53 +0530

    Update running_order_model.dart

--- lib/Model/running_order_model.dart
+++ lib/Model/running_order_model.dart
@@ -1,35 +1,40 @@
+import 'package:intl/intl.dart';
+
 class RunningOrderModel {
+  final String id;
   final String? name;
   final String? mobileNo;
   final String? salePointType;
   final String? salePointName;
   final String? waiterName;
   final String? waiterMobileNumber;
-  final double? amount;
+  final double amount;
   final int? pax;
   final String? activeSince;
-  final bool? billPrinted;
+  final bool billPrinted;
   final String? outletName;
   String? outletId;
   final String? masterFilter;
-
   RunningOrderModel(
-      {this.name,
+      {required this.id,
+      this.name,
       this.mobileNo,
       this.salePointType,
       this.salePointName,
       this.waiterName,
       this.waiterMobileNumber,
-      this.amount,
+      required this.amount,
       this.pax,
       this.activeSince,
-      this.billPrinted,
+      required this.billPrinted,
       this.outletName,
       this.outletId,
       this.masterFilter});
 
   factory RunningOrderModel.fromJson(Map<String, dynamic> json) {
+    var format = DateFormat('d/M/y, hh:mm');
     return RunningOrderModel(
+      id: json['id'],
       name: json['Name'],
       mobileNo: json['MobileNo'],
       salePointType: json['SalePointType'],
@@ -38,9 +43,10 @@ class RunningOrderModel {
       waiterMobileNumber: json['WaiterMobileNo'],
       amount: double.parse('${json['Amount']}'),
       pax: json['PAX'],
-      activeSince: json['ActiveSince'],
+      activeSince: format.format(DateTime.parse(json['ActiveSince'])),
       billPrinted: json['BillPrinted'] == 1,
       outletName: json['OutletName'],
+      outletId: json['OutletId'],
       masterFilter: json['Name'] +
           json['MobileNo'] +
           json['WaiterName'] +
