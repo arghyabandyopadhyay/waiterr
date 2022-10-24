c3f7538f1f4d02bee0f51f68d45ad12137373f2c
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-24 18:50:50 +0530

    Update kot_model.dart

--- lib/Model/kot_model.dart
+++ lib/Model/kot_model.dart
@@ -8,28 +8,50 @@ class KOTModel {
   final bool? discountable;
   final double? discountPercent;
   final double? taxRate;
-  final bool? orderPlaced;
-  final bool? orderApproved;
-  final bool? orderPrepared;
-  final bool? orderProcessed;
+  final bool orderPlaced;
+  final bool orderApproved;
+  final bool orderPrepared;
+  final bool orderProcessed;
+  final String? runningOrderId;
+  final String? outletName;
+  final String? orderId;
 
-  KOTModel({this.salePointType, this.salePointName, this.kotNumber, this.item,this.quantity, this.rate,this.discountable, this.discountPercent,this.taxRate, this.orderPlaced, this.orderApproved, this.orderPrepared, this.orderProcessed});
+  KOTModel(
+      {this.salePointType,
+      this.salePointName,
+      this.kotNumber,
+      this.item,
+      this.quantity,
+      this.rate,
+      this.discountable,
+      this.discountPercent,
+      this.taxRate,
+      this.runningOrderId,
+      this.outletName,
+      this.orderId,
+      required this.orderPlaced,
+      required this.orderApproved,
+      required this.orderPrepared,
+      required this.orderProcessed});
 
   factory KOTModel.fromJson(Map<String, dynamic> json) {
     return KOTModel(
-        salePointType: json['SalePointType'],
-        salePointName: json['SalePointName'],
-        kotNumber:json['KotNumber'],
-        item:json['Item'],
-        quantity:json['Quantity'],
-        rate:json['Rate'],
-        discountable:json['Discountable'],
-        discountPercent:json['DiscountPercent'],
-        taxRate:json['TaxRate'],
-        orderPlaced:json['OrderPlaced'],
-        orderApproved:json['OrderApproved'],
-        orderPrepared:json['OrderPrepared'],
-        orderProcessed:json['OrderProcessed'],
+      salePointType: json['SalePointType'],
+      salePointName: json['SalePointName'],
+      kotNumber: json['KotNumber'],
+      runningOrderId: json['RunningOrderId'],
+      outletName: json['OutletName'],
+      orderId: json['OrderId'],
+      item: json['Item'],
+      quantity: double.parse(json['Quantity'].toString()),
+      rate: double.parse(json['Rate'].toString()),
+      discountable: json['Discountable'] == 1,
+      discountPercent: double.parse(json['DiscountPercent'].toString()),
+      taxRate: double.parse(json['TaxRate'].toString()),
+      orderPlaced: json['OrderPlaced'] == 1,
+      orderApproved: json['OrderApproved'] == 1,
+      orderPrepared: json['OrderPrepared'] == 1,
+      orderProcessed: json['OrderProcessed'] == 1,
     );
   }
 }
