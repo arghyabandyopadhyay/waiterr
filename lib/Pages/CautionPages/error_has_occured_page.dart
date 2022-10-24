2a71460863b52e5def6d3ac0879c70a4fa834d7c
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-24 18:51:26 +0530

    Update error_has_occured_page.dart

--- lib/Pages/CautionPages/error_has_occured_page.dart
+++ lib/Pages/CautionPages/error_has_occured_page.dart
@@ -110,7 +110,6 @@ class _ErrorHasOccuredPageState extends State<ErrorHasOccuredPage> {
                       padding: EdgeInsets.symmetric(horizontal: 15),
                       child: Text(
                         "Waiterr",
-                        textScaleFactor: 1,
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                         style: TextStyle(
