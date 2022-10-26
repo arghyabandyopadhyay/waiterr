af99241aae31896879633ab348d031dab5e39581
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-26 22:18:38 +0530

    Create option_modal_bottom_sheet.dart

---
+++ lib/widgets/option_modal_bottom_sheet.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../Model/model_option_model.dart';
+
+class OptionModalBottomSheet extends StatelessWidget {
+  final String appBarText;
+  final IconData appBarIcon;
+  final List<ModalOptionModel> list;
+  const OptionModalBottomSheet(
+      {Key? key,
+      required this.list,
+      required this.appBarText,
+      required this.appBarIcon})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(appBarText),
+        leading: IconButton(
+          icon: Icon(appBarIcon),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(list[index].particulars),
+                  leading: Icon(
+                    list[index].icon,
+                    color: list[index].iconColor,
+                  ),
+                  onTap: list[index].onTap,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
