4f9aa7946ce047d93627f1148b2ff9c8c9684576
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-28 00:05:09 +0530

    Create waiter_details_model.dart

---
+++ lib/Model/waiter_details_model.dart
@@ -0,0 +1,47 @@
+class WaiterDetailsModel {
+  final String id;
+  final String name;
+  final String mobileNumber;
+  final int roleId;
+  final bool isActive;
+  final String outletId;
+  final String lastLogin;
+  final String outletName;
+  final String? masterFilter;
+
+  WaiterDetailsModel(
+      {required this.id,
+      required this.name,
+      required this.mobileNumber,
+      required this.roleId,
+      required this.isActive,
+      required this.outletId,
+      required this.lastLogin,
+      required this.outletName,
+      required this.masterFilter});
+  factory WaiterDetailsModel.fromJson(Map<String, dynamic> json) {
+    return WaiterDetailsModel(
+        id: json['id'],
+        name: json['Name'],
+        mobileNumber: json['MobileNumber'],
+        roleId: json['RoleId'],
+        isActive: json['IsActive'] == 1,
+        lastLogin: json['last_login'],
+        outletId: json['OutletId'],
+        outletName: json['OutletName'],
+        masterFilter: json['MasterFilter']);
+  }
+
+  Map toJson() {
+    return {
+      "id": id,
+      "Name": name,
+      "MobileNumber": mobileNumber,
+      "RoleId": roleId,
+      "IsActive": isActive ? 1 : 0,
+      "last_login": lastLogin,
+      "OutletId": outletId,
+      "OutletName": outletName
+    };
+  }
+}
