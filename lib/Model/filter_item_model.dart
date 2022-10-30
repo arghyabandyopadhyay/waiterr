77ef5d5d47bdfabf4afd4d72d948d0ad3392a30d
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-10-30 16:20:16 +0530

    Update filter_item_model.dart

--- lib/Model/filter_item_model.dart
+++ lib/Model/filter_item_model.dart
@@ -1,11 +1,14 @@
 class FilterItemModel {
+  final String id;
   final String? image;
   final String? stockGroup;
   final String? masterFilter;
-  FilterItemModel({this.image, this.stockGroup, this.masterFilter});
+  FilterItemModel(
+      {required this.id, this.image, this.stockGroup, this.masterFilter});
 
   factory FilterItemModel.fromJson(Map<String, dynamic> json) {
     return FilterItemModel(
+      id: json['id'],
       //image: json['Image']!=null&&json['Image']!=""?Image.memory(base64Decode(json['Image'])):Image.asset('assets/img/all_filter_icon.png'),
       image: json['ImageUrl'],
       stockGroup: json['StockGroup'],
