abb013208939c16d59301406077a84ffd7ba3bef
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-11-15 23:56:06 +0530

    Update waiter_manager_page.dart

--- lib/Pages/Restaurant/Management/waiter_manager_page.dart
+++ lib/Pages/Restaurant/Management/waiter_manager_page.dart
@@ -107,7 +107,6 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
   Widget build(BuildContext context) {
     return Consumer<LoginStore>(builder: (_, loginStore, __) {
       return Scaffold(
-        backgroundColor: GlobalTheme.backgroundColor,
         resizeToAvoidBottomInset: false,
         body: Stack(
           children: [
@@ -142,7 +141,6 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
                               decoration: const InputDecoration(
                                   prefixIcon: Icon(
                                     Icons.search,
-                                    color: GlobalTheme.primaryText,
                                   ),
                                   border: OutlineInputBorder(
                                       borderSide: BorderSide.none),
@@ -185,7 +183,7 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
                   )
                 ],
               ),
-              backgroundColor: GlobalTheme.backgroundColor.withOpacity(0.7),
+              backgroundColor: GlobalTheme.tint,
               body: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
@@ -207,24 +205,7 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
                         child: Container(
                             height: MediaQuery.of(context).size.height,
                             padding: const EdgeInsets.only(top: 10),
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(30),
-                                topRight: Radius.circular(30),
-                              ),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: GlobalTheme.primaryText,
-                                  blurRadius: 25.0, // soften the shadow
-                                  spreadRadius: 5.0, //extend the shadow
-                                  offset: Offset(
-                                    15.0, // Move to right 10  horizontally
-                                    15.0, // Move to bottom 10 Vertically
-                                  ),
-                                )
-                              ],
-                            ),
+                            decoration: GlobalTheme.waiterrAppBarBoxDecoration,
                             child: FutureBuilder<List<WaiterDetailsModel>>(
                                 future: _futurewaiters,
                                 builder: (context, snapshot) {
@@ -242,8 +223,7 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
                                             child: _isLoading!
                                                 ? const CircularProgressIndicator(
                                                     strokeWidth: 3,
-                                                    backgroundColor: GlobalTheme
-                                                        .progressBarBackground)
+                                                  )
                                                 : null,
                                           ),
                                         ),
@@ -482,7 +462,6 @@ class _WaiterManagerPageState extends State<WaiterManagerPage> {
                 onPressed: _addWaiter,
                 child: const Icon(
                   Icons.add,
-                  color: GlobalTheme.floatingButtonText,
                 ),
               ),
               floatingActionButtonLocation:
