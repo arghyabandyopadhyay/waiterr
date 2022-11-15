121371fb3ebae7718cffe25f7b638db5086867c7
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-11-15 23:58:26 +0530

    Update favorite_card.dart

--- lib/widgets/favorite_card.dart
+++ lib/widgets/favorite_card.dart
@@ -24,163 +24,6 @@ class FavouritesCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 7,
-      /*child: Container(
-          width: 300,
-          margin: EdgeInsets.symmetric(vertical: 10),
-          padding: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.only(bottom: 2),
-                        child: item.IsVeg? new Image.asset('assets/img/veg.png',height: 15,width: 15,):new Image.asset('assets/img/nonveg.png',height: 15,width: 15,),
-                      ),
-                      SizedBox(width: 5,),
-                      Container(
-                        width: MediaQuery.of(context).size.width/3,
-                        child: Text(
-                          item.item,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          
-                          style: TextStyle( fontSize: 17),
-                        ),
-                      ),
-                      SizedBox(width: 10,),
-                      Container(
-                        height: (isNumeric(item.rate.toString()))?0:null,
-                        padding: EdgeInsets.only(left: 1,right: 1),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Colors.orange,width: 1.0,style: BorderStyle.solid)
-                        ),
-                        child: Text(" Customizable ",
-                          
-                          style: TextStyle(fontSize: 10,color: Colors.orange),
-                        ),
-                      )
-                    ],
-                  ),
-                  Text(
-                    //!isNumeric(item.rate.toString())?("₹"+item.rate[0]['Price']):("₹"+item.rate),
-                    "₹"+item.rate.toString(),
-                    textAlign: TextAlign.end,
-                    
-                    style: TextStyle( fontSize: 15),
-                  ),
-
-                ],
-              ),
-
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width/2,
-                    child:
-                    Text(
-                      (item.itemDescription!=null)?item.itemDescription:"Add sweet corn, and salt to the wok and mix",
-                      
-                      style: TextStyle( color:GlobalTheme.primaryText,fontSize: 13,),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                    ),
-                  ),
-                  /*
-                  Container(
-                    padding: item['Quantity']=="0"?EdgeInsets.symmetric(horizontal: 13.5,vertical: 2.5):EdgeInsets.all(0),
-                    alignment: Alignment.bottomRight,
-                    decoration: BoxDecoration(
-                      gradient: item['Quantity']=="0"?LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: GlobalTheme.primaryGradient):null,
-                      borderRadius: BorderRadius.circular(20.0),
-                      border: Border.all(color: GlobalTheme.primaryColor,width: 1,style: BorderStyle.solid),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          child: item['Quantity']=="0"?GestureDetector(
-                              child: Text("Add",style: TextStyle(fontSize:12,color: Colors.white),),
-                              onTap: (){
-                                onTapAdd();
-                              },
-                              onLongPress: (){
-                                onLongPressedAdd();
-                              }
-                          ):GestureDetector(
-                            child: Icon(Icons.remove,size: 17,),
-                            onTap: (){
-                              onTapRemove();
-                            },
-                            onLongPress: (){
-                              onLongPressedRemove();
-                            },
-                          ),
-                          padding: EdgeInsets.all(1),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(0),
-                          width: item['Quantity']=="0"?0:20,
-                          height: item['Quantity']=="0"?0:25,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: GlobalTheme.primaryGradient),
-                          ),
-                          child: Center(
-                            child: GestureDetector(
-                              child: Text(
-                                item['Quantity'],
-                                
-                                style: TextStyle(
-                                    fontSize: 12.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w300),
-                              ),
-                              onTap: (){
-                                onTapAdd();
-                              },
-                            ),
-                          ),
-                        ),
-                        Container(
-                          child: GestureDetector(
-                              child: item['Quantity']=="0"?Text("",):Icon(Icons.add,size: 17,color: GlobalTheme.primaryColor,),
-                              onTap: (){
-                                onTapAdd();
-                              },
-                              onLongPress: (){
-                                onLongPressedAdd();
-                              }
-                          ),
-                          padding: EdgeInsets.all(1),
-                        ),
-                      ],
-                    ),
-                  ),*/
-                  AddButton(
-                    item: item,
-                    onLongPressedRemove:onLongPressedRemove ,
-                    onLongPressedAdd:onLongPressedAdd ,
-                    onTapRemove: onTapRemove,
-                    onTapAdd: onTapAdd,
-                  )
-                ],
-              ),
-
-
-            ],
-          )
-      ),*/
       child: GestureDetector(
         onDoubleTap: onDoubleTap,
         child: Container(
@@ -276,7 +119,6 @@ class FavouritesCard extends StatelessWidget {
                             maxLines: 1,
                             style: const TextStyle(
                                 fontSize: 15,
-                                color: GlobalTheme.primaryText,
                                 decoration: TextDecoration.lineThrough),
                           )
                         ],
@@ -297,7 +139,6 @@ class FavouritesCard extends StatelessWidget {
                       ? Text(
                           item.itemDescription!,
                           style: const TextStyle(
-                            color: GlobalTheme.primaryText,
                             fontSize: 13,
                           ),
                           maxLines: 1,
