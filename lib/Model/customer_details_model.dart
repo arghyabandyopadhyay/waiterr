82ec165ea2991b556a74219fd050c0d39d9aded7
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-12-01 19:19:06 +0530

    Update customer_details_model.dart

--- lib/Model/customer_details_model.dart
+++ lib/Model/customer_details_model.dart
@@ -1,23 +1,30 @@
 //CustomerDetailsModel
 class CustomerDetailsModel {
-  final String? customerId;
-  final String? name;
-  final String? mobileNumber;
+  final String customerId;
+  final String name;
+  final String mobileNumber;
+  final String dataSource;
 
-  CustomerDetailsModel({this.customerId, this.name, this.mobileNumber});
+  CustomerDetailsModel(
+      {required this.customerId,
+      required this.name,
+      required this.mobileNumber,
+      required this.dataSource});
 
   factory CustomerDetailsModel.fromJson(Map<String, dynamic> json) {
     return CustomerDetailsModel(
-      customerId: json['id'],
-      name: json['Name'],
-      mobileNumber: json['MobileNumber'],
-    );
+        customerId: json['id'],
+        name: json['Name'],
+        mobileNumber: json['MobileNumber'],
+        dataSource: json['DataSource']);
   }
 
   Map<String, dynamic> toJson() {
     return {
+      "id": customerId,
       "Name": name,
       "MobileNumber": mobileNumber,
+      "DataSource": dataSource
     };
   }
 }
