83068f3127544aff2c52a822565030b45b549e6b
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-01-13 01:40:44 +0530

    Create menu_manager_list.dart

---
+++ lib/widgets/menu_manager_list.dart
@@ -0,0 +1,42 @@
+import 'package:waiterr/Model/menu_item_model.dart';
+import 'package:expandable_group/expandable_group_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:waiterr/theme.dart';
+
+class MenuManagerList extends StatefulWidget {
+  List<List<MenuItemModel>>? productList;
+  Function? header;
+  Function? buildItems;
+  MenuManagerList({Key? key, this.productList, this.buildItems, this.header})
+      : super(key: key);
+  @override
+  State<MenuManagerList> createState() => _MenuManagerListState();
+}
+
+class _MenuManagerListState extends State<MenuManagerList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: widget.productList!.map((group) {
+      int index = widget.productList!.indexOf(group);
+      return ExpandableGroup(
+        expandedIcon: const Icon(
+          Icons.arrow_drop_down,
+          size: 40,
+          color: GlobalTheme.waiterrPrimaryColor,
+        ),
+        collapsedIcon: const Icon(
+          Icons.arrow_right,
+          size: 40,
+          color: GlobalTheme.waiterrPrimaryColor,
+        ),
+        isExpanded: true,
+        header: widget.header!(widget.productList![index][0].outletName),
+        items: widget.buildItems!(context, group),
+        headerEdgeInsets: const EdgeInsets.only(left: 16.0, right: 16.0),
+      );
+    }).toList());
+  }
+}
