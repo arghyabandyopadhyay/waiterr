d1b5b0837accb6a81284c4823a1ab11507eed601
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-01-13 01:41:24 +0530

    Update generated_plugin_registrant.dart

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,8 +7,6 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
-import 'package:connectivity_plus_web/connectivity_plus_web.dart';
-import 'package:device_info_plus_web/device_info_plus_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
@@ -17,14 +15,11 @@ import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
-  ConnectivityPlusPlugin.registerWith(registrar);
-  DeviceInfoPlusPlugin.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseDatabaseWeb.registerWith(registrar);
