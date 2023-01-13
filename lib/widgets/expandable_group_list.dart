463d08ea7566e66b48a324a1283d91b25dcb2d0f
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-01-13 23:06:02 +0530

    Create expandable_group_list.dart

---
+++ lib/widgets/expandable_group_list.dart
@@ -0,0 +1,43 @@
+import 'package:waiterr/Model/menu_item_model.dart';
+import 'package:expandable_group/expandable_group_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:waiterr/Model/waiter_details_model.dart';
+import 'package:waiterr/theme.dart';
+
+class ExpandableGroupList<T> extends StatefulWidget {
+  final List<List<T>>? list;
+  final Function? header;
+  final Function? buildItems;
+  const ExpandableGroupList({Key? key, this.list, this.buildItems, this.header})
+      : super(key: key);
+  @override
+  State<ExpandableGroupList> createState() => _ExpandableGroupListState();
+}
+
+class _ExpandableGroupListState extends State<ExpandableGroupList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: widget.list!.map((group) {
+      int index = widget.list!.indexOf(group);
+      return ExpandableGroup(
+        expandedIcon: const Icon(
+          Icons.arrow_drop_down,
+          size: 40,
+          color: GlobalTheme.waiterrPrimaryColor,
+        ),
+        collapsedIcon: const Icon(
+          Icons.arrow_right,
+          size: 40,
+          color: GlobalTheme.waiterrPrimaryColor,
+        ),
+        isExpanded: true,
+        header: widget.header!(widget.list![index][0].outletName),
+        items: widget.buildItems!(context, group),
+        headerEdgeInsets: const EdgeInsets.only(left: 16.0, right: 16.0),
+      );
+    }).toList());
+  }
+}
