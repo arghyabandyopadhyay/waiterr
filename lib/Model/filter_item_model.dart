204edc56e4db1ccdb59d2b4502f42dd231c262c3
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-01-13 23:06:35 +0530

    Update filter_item_model.dart

--- lib/Model/filter_item_model.dart
+++ lib/Model/filter_item_model.dart
@@ -3,19 +3,25 @@ class FilterItemModel {
   final String? image;
   final String? stockGroup;
   final String masterFilter;
-  FilterItemModel(
-      {required this.id,
-      this.image,
-      this.stockGroup,
-      required this.masterFilter});
+  final String outletId;
+  final String outletName;
+  FilterItemModel({
+    required this.id,
+    this.image,
+    this.stockGroup,
+    required this.masterFilter,
+    required this.outletId,
+    required this.outletName,
+  });
 
   factory FilterItemModel.fromJson(Map<String, dynamic> json) {
     return FilterItemModel(
-      id: json['id'],
-      //image: json['Image']!=null&&json['Image']!=""?Image.memory(base64Decode(json['Image'])):Image.asset('assets/img/all_filter_icon.png'),
-      image: json['ImageUrl'],
-      stockGroup: json['StockGroup'],
-      masterFilter: json['MasterFilter'] ?? json['StockGroup'],
-    );
+        id: json['id'],
+        //image: json['Image']!=null&&json['Image']!=""?Image.memory(base64Decode(json['Image'])):Image.asset('assets/img/all_filter_icon.png'),
+        image: json['ImageUrl'],
+        stockGroup: json['StockGroup'],
+        masterFilter: json['MasterFilter'] ?? json['StockGroup'],
+        outletId: json['OutletId'],
+        outletName: json['OutletName']);
   }
 }
