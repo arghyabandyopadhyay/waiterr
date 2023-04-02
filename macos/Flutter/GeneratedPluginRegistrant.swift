691889ce389c67e21692705d831022ff39195d08
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-04-02 12:25:53 +0530

    gitignore update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,8 +11,8 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import flutter_secure_storage_macos
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
