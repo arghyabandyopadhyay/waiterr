bbe04bf21ce18cfaf14ce1a37898da5c3d43b27a
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2023-09-18 22:02:50 +0530

    Update profile_image_widget.dart

--- lib/widgets/profile_image_widget.dart
+++ lib/widgets/profile_image_widget.dart
@@ -1,20 +1,43 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart' as firebase_core;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:waiterr/Modules/api_fetch_module.dart';
+import 'package:waiterr/Pages/Restaurant/Management/add_image_page.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
+import '../Modules/firebase_storage_module.dart';
+import '../Modules/universal_module.dart';
 import '../global_class.dart';
 
-class ProfileImageWidget extends StatelessWidget {
+firebase_storage.FirebaseStorage storage =
+    firebase_storage.FirebaseStorage.instance;
+
+class ProfileImageWidget extends StatefulWidget {
   final double radius;
   final Size size;
-  const ProfileImageWidget({Key? key, required this.radius, required this.size})
+  final bool isImageUploader;
+  final GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey;
+  const ProfileImageWidget(
+      {Key? key,
+      required this.radius,
+      required this.size,
+      required this.isImageUploader,
+      this.scaffoldMessengerKey})
       : super(key: key);
+  @override
+  State<ProfileImageWidget> createState() => _ProfileImageWidgetState();
+}
+
+class _ProfileImageWidgetState extends State<ProfileImageWidget> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       child: CircleAvatar(
-          radius: radius,
+          radius: widget.radius,
           backgroundColor: Colors.transparent,
           child: ClipOval(
             child: UserDetail.userDetails.profileUrl != null &&
@@ -22,8 +45,8 @@ class ProfileImageWidget extends StatelessWidget {
                 ? CachedNetworkImage(
                     imageUrl: UserDetail.userDetails.profileUrl!,
                     imageBuilder: (context, imageProvider) => Container(
-                      height: size.height,
-                      width: size.width,
+                      height: widget.size.height,
+                      width: widget.size.width,
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: imageProvider,
@@ -49,16 +72,88 @@ class ProfileImageWidget extends StatelessWidget {
                         const Icon(Icons.error),
                   )
                 : Image(
-                    height: size.height,
-                    width: size.width,
+                    height: widget.size.height,
+                    width: widget.size.width,
                     fit: BoxFit.fitHeight,
                     image: Image.asset(
                       'assets/img/profile.png',
                     ).image),
           )),
       onTap: () {
-        if (kDebugMode) print("profile");
+        if (widget.isImageUploader) {
+          Navigator.of(context).push(CupertinoPageRoute<void>(
+            title: "Profile Page",
+            builder: (context) => AddImagePage(callback: uploadFile),
+          ));
+        }
       },
     );
+  }
+
+  double progressValue = 0;
+  bool isUploading = false;
+  Future<void> uploadFile(String filePath) async {
+    try {
+      progressValue = 0;
+      String profileThumbnailDirectory =
+          'ProfilePictures/${UserDetail.userDetails.id}';
+      setState(() {
+        isUploading = true;
+      });
+      File thumbnailFile = File(filePath);
+      firebase_storage.UploadTask task = storage
+          .ref(profileThumbnailDirectory)
+          .putFile(thumbnailFile, firebase_storage.SettableMetadata());
+      task.snapshotEvents.listen((firebase_storage.TaskSnapshot snapshot) {
+        if (mounted) {
+          setState(() {
+            progressValue = (snapshot.bytesTransferred / snapshot.totalBytes);
+          });
+        }
+      }, onError: (e) {
+        // The final snapshot is also available on the task via `.snapshot`,
+        // this can include 2 additional states, `TaskState.error` & `TaskState.canceled`
+        globalShowInSnackBar(
+            e.toString(),
+            null,
+            widget.scaffoldMessengerKey ?? GlobalKey<ScaffoldMessengerState>(),
+            null,
+            null);
+        if (e.code == 'permission-denied') {
+          globalShowInSnackBar(
+              'User does not have permission to upload to this reference.',
+              null,
+              widget.scaffoldMessengerKey ??
+                  GlobalKey<ScaffoldMessengerState>(),
+              null,
+              null);
+        }
+      });
+      await task;
+      UserDetail.userDetails.profileUrl =
+          await downloadURL(profileThumbnailDirectory);
+      await putRegistrationDetails(UserDetail.userDetails);
+      globalShowInSnackBar(
+          "Upload complete!!",
+          null,
+          widget.scaffoldMessengerKey ?? GlobalKey<ScaffoldMessengerState>(),
+          null,
+          null);
+      if (mounted) {
+        setState(() {
+          isUploading = false;
+        });
+      }
+    } on firebase_core.FirebaseException catch (e) {
+      if (e.code == 'permission-denied') {
+        globalShowInSnackBar(
+            'User does not have permission to upload to this reference.',
+            null,
+            widget.scaffoldMessengerKey ?? GlobalKey<ScaffoldMessengerState>(),
+            null,
+            null);
+      }
+      // ...
+    }
   }
 }
